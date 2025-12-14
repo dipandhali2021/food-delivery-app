881a5f302b519922bc037a3bc5fb78e490a3b1b1
Dipan Dhali <dipandhali2021@gmail.com>
2025-12-14 20:30:12 +0530

    Refactor code structure for improved readability and maintainability

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,80 @@
-import { offers } from '@/constants';
+import { images, offers } from '@/constants';
 import { Fragment } from 'react';
-import { FlatList, Image, Pressable, Text, View } from 'react-native';
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import clsx from 'clsx';
+import CartButton from '@/components/CartButton';
 
 export default function Index() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
       <FlatList
         data={offers}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 px-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Crotia</Text>
+
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                  tintColor="#000000"
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton />
+          </View>
+        )}
         renderItem={({ item, index }) => {
           const isEven = index % 2 === 0;
           return (
             <View key={index}>
-              <Pressable className="offer-card " style={{ backgroundColor: item.color }}>
+              <Pressable
+                className={clsx(
+                  'offer-card',
+                  isEven ? 'flex-row-reverse' : 'flex-row'
+                )}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: '#Fffff22' }}
+              >
                 {({ pressed }) => (
                   <Fragment>
-                   <View className="h-full w-1/2">
-                   <Image
-                      source={item.image}
-                      className="size-full"
-                      resizeMode="contain"
-                   />
-
-                   </View>
-                   <View className='offer-card__info'>
-                    <Text className="offer-card__title">{item.title}</Text>
-                   </View>
+                    <View className="h-full w-1/2">
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+                    <View
+                      className={clsx(
+                        'offer-card__info',
+                        isEven ? 'pl-10' : 'pr-10'
+                      )}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        resizeMode="contain"
+                        className="size-10"
+                        tintColor="#ffffff"
+                      />
+                    </View>
                   </Fragment>
-                ) }
+                )}
               </Pressable>
             </View>
           );
