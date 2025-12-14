769cbfb03c58dbbd9b241c5682ec0ce086ac49c8
Dipan Dhali <dipandhali2021@gmail.com>
2025-12-14 22:50:34 +0530

    feat: integrate Sentry for error tracking and monitoring

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,31 @@ import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 
 import "./globals.css";
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({
+  dsn: 'https://534ce2d7cb4dbacac364cfdf284c0170@o4510534314491904.ingest.us.sentry.io/4510534315933696',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 // Prevent the splash screen from auto-hiding before asset loading is complete
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded,error] = useFonts({
     "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
     "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
@@ -26,4 +46,4 @@ export default function RootLayout() {
   }
 
   return <Stack  screenOptions={{headerShown:false}}/>;
-}
+});
